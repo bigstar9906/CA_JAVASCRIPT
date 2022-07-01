cf763ce249ea30554614201cb301c68bbcab4c54
bigstar9906 <bigstar9906@naver.com>
2022-07-02 00:47:56 +0900

    Assignment 9

--- tab.js
+++ tab.js
@@ -1,27 +1,18 @@
 var current_tab=1;
 var button = $('.tab-button');
 var content = $('.tab-content');
-for(let i=0;i<3;i++)
+
+
+$('.list').click(function(e){
+    open_tab(parseInt( e.target.dataset.id));
+    
+});
+
+function open_tab(num)
 {
-button.eq(i).on('click',function(){
     button.eq(current_tab).removeClass('orange');
     content.eq(current_tab).removeClass('show');
-    button.eq(i).addClass('orange');
-    content.eq(i).addClass('show');
-    current_tab = i;
-});
+    button.eq(num).addClass('orange');
+    content.eq(num).addClass('show');
+    current_tab = num;
 }
-// button.eq(1).on('click',function(){
-//     button.eq(current_tab).removeClass('orange');
-//     content.eq(current_tab).removeClass('show');
-//     button.eq(1).addClass('orange');
-//     content.eq(1).addClass('show');
-//     current_tab = 1;
-// });
-// button.eq(2).on('click',function(){
-//     button.eq(current_tab).removeClass('orange');
-//     content.eq(current_tab).removeClass('show');
-//     button.eq(2).addClass('orange');
-//     content.eq(2).addClass('show');
-//     current_tab = 2;
-// });
